ee7eb86590ede8ababf19c784682696b5452ecdb
anjali <anjalikkapoor2001@gmail.com>
2020-05-25 03:21:49 -0400

    Added hooks to formspree

--- index.js
+++ index.js
@@ -172,13 +172,28 @@ function animate() {
 animate();
 
 $('#Form').on('submit', function() {
-    // message = $('#contactform').serialize();
-    // $.ajax({
-    //     url: 'https://formspree.io/myynekpl', 
-    //     method: 'POST',
-    //     data: {message: message},
-    //     dataType: 'json'
-    // });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    return false;
+	let message = $('#EmailInput').val();
+	let request = $.ajax({
+		url: 'https://formspree.io/myynekpl', 
+		method: 'POST',
+		data: {message: message},
+		dataType: 'json'
+	});
+	request.done((msg) => {
+		console.log(msg);
+		// Run success.
+		$('#Form').fadeOut(() => {
+			$('#Status').text('Thanks! Keep an eye on your inbox for the latest updates!');
+			$('#Status').fadeIn();
+		});
+	});
+	request.fail((jqXHR, textStatus) => {
+		console.log(textStatus);
+		// Run failure.
+		$('#Form').fadeOut(() => {
+			$('#Status').text('Looks like there was a problem. Get in touch by email at...');
+			$('#Status').fadeIn();
+		});
+	});
+	return false;
 });
